5a62c8cc5fd34fc7021c900cfb38056ecc41cf3c
soumyaprasadrana <soumyaprasad.rana@gmail.com>
2021-09-13 22:22:23 +0530

    add update vm, add update user, add team and team lead

---
+++ src/app/public/services/properties.services.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { VM } from '../DataModel/vm';
+import { Observable, of } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+import { NodeclientService } from './nodeclient.service';
+@Injectable({
+  providedIn: 'root',
+})
+/* VM Management System Services it will include all vm related services */
+export class UIPropService {
+  vms: Array<any> = [];
+  promiseX: any;
+  observable: any;
+  constructor(private _client: NodeclientService) {}
+
+  getDataFromNode(): any {
+    var headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    var httpOptions = {
+      headers: headers,
+    };
+    var promise = this._client.get('api/public/getUIProps', httpOptions);
+    return promise;
+  }
+}
