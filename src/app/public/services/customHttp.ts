f950ac45b41cd9d57efc5de2efd32dbf1389dd8a
Soumya Prasad Rana <soumyaprasad.rana@gmail.com>
2022-04-28 16:17:42 +0530

    Network Error Fixed

--- src/app/public/services/customHttp.ts
+++ src/app/public/services/customHttp.ts
@@ -24,12 +24,14 @@ import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { SpinnerService } from './spinner-service';
 import { filter } from 'rxjs/operators';
 import { MATH_PIPES } from 'ngx-pipes';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertDialogComponent } from '../widget/alert-dialog/alert-dialog.component';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   previousUrl: string = '';
   currentUrl: string = '';
-  constructor(private router: Router, private route: ActivatedRoute) {
+  constructor(private router: Router, private route: ActivatedRoute,private dialog:MatDialog) {
     this.router.events
       .pipe(filter((event) => event instanceof NavigationEnd))
       .subscribe((event: any) => {
@@ -39,7 +41,21 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
+
     //handle your auth error or rethrow
+    if(err.status===0 && !err.ok){
+      this.openDialog(
+        {
+          type: 'alert',
+          message: 'Error occurred:'+ (/Http failure response/.test(err.message))?'Network Error!':err.message,
+        },
+        ()=>{
+          if(/Http failure response/.test(err.message)){
+            window.location.reload()
+          }
+        }
+      );
+    }
     if (err.status === 401 || err.status === 403) {
       //navigate /delete cookies or whatever
       this.router.navigateByUrl(`/portal/login`).then(() => {
@@ -72,5 +88,19 @@ export class AuthInterceptor implements HttpInterceptor {
 
     // catch the error, make specific functions for catching specific errors and you can chain through them with more catch operators
     return next.handle(req).pipe(catchError((x) => this.handleAuthError(x))); //here use an arrow function, otherwise you may get "Cannot read property 'navigate' of undefined" on angular 4.4.2/net core 2/webpack 2.70
+  }
+  openDialog(data: any, callback: any) {
+    this.dialog
+      .open(AlertDialogComponent, {
+        data: data,
+        panelClass: 'app-dialog-class',
+      })
+      .afterClosed()
+      .toPromise()
+      .then((res) => {
+        if (typeof callback == 'function') {
+          callback();
+        }
+      });
   }
 }
