27f9899b2bad01d3d788d3b7d5781ff7cf1b92a4
Soumya Prasad Rana <soumyaprasad.rana@gmail.com>
2022-11-27 09:34:43 +0530

    ADDITIONAL DATA BUG FIX, XTERM CROOCKED TEXT, SSH DEFAULT PASSWORD BLANK

--- src/app/public/tools/tools-ltb/tools-ltb.component.ts
+++ src/app/public/tools/tools-ltb/tools-ltb.component.ts
@@ -9,34 +9,38 @@
  * @modify date 2022-02-26 18:26:41
  * @desc Tools LTB Component
  */
-import { HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { NodeclientService } from '../../services/nodeclient.service';
+import { HttpHeaders } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { NodeclientService } from "../../services/nodeclient.service";
 @Component({
-  selector: 'app-tools-ltb',
-  templateUrl: './tools-ltb.component.html',
-  styleUrls: ['./tools-ltb.component.scss'],
+  selector: "app-tools-ltb",
+  templateUrl: "./tools-ltb.component.html",
+  styleUrls: [ "./tools-ltb.component.scss" ],
 })
 export class ToolsLtbComponent implements OnInit {
   cardsMetaData: any;
   selectedDataContext: any;
   defaultSSHUsername: any;
   defaultSSHPassword: any;
-  constructor(private _client: NodeclientService,private actRoute: ActivatedRoute, private router: Router) {
+  constructor(
+    private _client: NodeclientService,
+    private actRoute: ActivatedRoute,
+    private router: Router
+  ) {
     //  this.cardsMetaData = ToolsLTBConfig.cardsMetaData;
-    if (typeof history.state.ip != 'undefined') {
+    if (typeof history.state.ip != "undefined") {
       this.selectedDataContext = history.state;
     }
     var headers = new HttpHeaders({
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     });
 
     var httpOptions = {
       headers: headers,
     };
-    this._client.get('api/public/sshMetadata', httpOptions).then(
+    this._client.get("api/public/sshMetadata", httpOptions).then(
       (res: any) => {
         this.defaultSSHUsername = res.defaultSSHUsername;
         this.defaultSSHPassword = res.defaultSSHPassword;
@@ -55,30 +59,44 @@ export class ToolsLtbComponent implements OnInit {
         //item
         var formItems = metadata[i][j].cardFields[1];
         //console.log(formItems);
-        if (typeof this.defaultSSHUsername != 'undefined') {
-          formItems['ssh_username'][0] = this.defaultSSHUsername;
+        if (typeof this.defaultSSHUsername != "undefined") {
+          formItems["ssh_username"][0] = this.defaultSSHUsername;
         }
-        if (typeof this.defaultSSHPassword != 'undefined') {
-          formItems['ssh_password'][0] = this.defaultSSHPassword;
+        if (typeof this.defaultSSHPassword != "undefined") {
+          formItems["ssh_password"][0] = this.defaultSSHPassword;
         }
         if (this.selectedDataContext) {
-          formItems['machine_ip'][0] = this.selectedDataContext.ip;
+          formItems["machine_ip"][0] = this.selectedDataContext.ip;
 
           if (
             this.selectedDataContext.extradata &&
-            typeof this.selectedDataContext.extradata.sshUsername != 'undefined'
+            typeof this.selectedDataContext.extradata.sshUsername != "undefined"
           ) {
-            formItems['ssh_username'][0] =
-              this.selectedDataContext.extradata.sshUsername;
+            formItems[
+              "ssh_username"
+            ][0] = this.selectedDataContext.extradata.sshUsername;
           }
-          if (typeof this.selectedDataContext.extradata != 'undefined') {
-            formItems['ssh_password'][0] =
-              this.selectedDataContext.extradata.sshPassword;
+          if (
+            typeof this.selectedDataContext.extradata != "undefined" &&
+            typeof this.selectedDataContext.extradata.sshPassword != "undefined"
+          ) {
+            formItems[
+              "ssh_password"
+            ][0] = this.selectedDataContext.extradata.sshPassword;
           }
         }
         for (var k in formItems) {
           //console.log('formItems[k][1]', formItems[k][1]);
-          if (formItems[k][1] == 'required') {
+          if (
+            formItems[k][0] != null &&
+            typeof formItems[k][0] == "string" &&
+            formItems[k][0].includes("{{") &&
+            formItems[k][0].includes("}}")
+          ) {
+            formItems[k][0] =
+              formItems[formItems[k][0].replace("{{", "").replace("}}", "")][0];
+          }
+          if (formItems[k][1] == "required") {
             formItems[k][1] = Validators.required;
             //console.log('formItems[k][1]: true', formItems[k][1]);
           }
@@ -89,41 +107,48 @@ export class ToolsLtbComponent implements OnInit {
     }
     return metadata;
   }
-  openWebSSH(){
+  openWebSSH() {
     console.log("clicked openWebSSH");
-   
-    var username=null;
-    var password=null;
-    var hostname=null;
-    var port=22;
-    if (typeof this.defaultSSHUsername != 'undefined') {
+
+    var username = null;
+    var password = null;
+    var hostname = null;
+    var port = 22;
+    if (typeof this.defaultSSHUsername != "undefined") {
       username = this.defaultSSHUsername;
     }
-    if (typeof this.defaultSSHPassword != 'undefined') {
+    if (typeof this.defaultSSHPassword != "undefined") {
       password = this.defaultSSHPassword;
     }
     if (this.selectedDataContext) {
       hostname = this.selectedDataContext.ip;
     }
-      if (
-        this.selectedDataContext.extradata &&
-        typeof this.selectedDataContext.extradata.sshUsername != 'undefined'
-      ) {
-        username =
-          this.selectedDataContext.extradata.sshUsername;
-      }
-      if (typeof this.selectedDataContext.extradata != 'undefined') {
-        password =
-          this.selectedDataContext.extradata.sshPassword;
-      }
-      const routerLink = ['/portal/spa/', 'sshclient'];
-      var appUrl = this.router.serializeUrl(
-        this.router.createUrlTree(routerLink,{ queryParams: { hostname: hostname, username: username,port:port,password:password } })
-      );
-   
+    if (
+      this.selectedDataContext.extradata &&
+      typeof this.selectedDataContext.extradata.sshUsername != "undefined"
+    ) {
+      username = this.selectedDataContext.extradata.sshUsername;
+    }
+    if (
+      typeof this.selectedDataContext.extradata != "undefined" &&
+      typeof this.selectedDataContext.extradata.sshPassword != "undefined"
+    ) {
+      password = this.selectedDataContext.extradata.sshPassword;
+    }
+    const routerLink = [ "/portal/spa/", "sshclient" ];
+    var appUrl = this.router.serializeUrl(
+      this.router.createUrlTree(routerLink, {
+        queryParams: {
+          hostname: hostname,
+          username: username,
+          port: port,
+          password: password,
+        },
+      })
+    );
+
     console.log(appUrl);
-    window.open(appUrl, '_blank');
-   
+    window.open(appUrl, "_blank");
   }
   ngOnInit(): void {}
 }
